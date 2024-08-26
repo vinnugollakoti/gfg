93b3f5315ec6c733c41b2ca36b9a55e33b4f2d0e
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-08-26 21:08:03 +0530

    added final touch

--- server/server.js
+++ server/server.js
@@ -89,8 +89,8 @@ console.log(req.body)
 
   const additionalLinks = {
       ML: "https://chat.whatsapp.com/CV1w56clzY4CbqZlXtxNOb",
-      DSA: "https://chat.whatsapp.com/HPFqcCNqPVjIHnYqMkpfHG",
-      GATE: "https://chat.whatsapp.com/EDmiXkYBuRq0wdGiQvyCSo"
+      DSA: "https://chat.whatsapp.com/E8eFaJJoBB373CtOnDaOfP",
+      GATE: "https://chat.whatsapp.com/DelDTfuUaWg67G688pnaRS"
   };
   const additionalPreferencesArray = JSON.parse(additionalPreferences || '[]');
   console.log('Parsed Additional Preferences:', additionalPreferencesArray);
@@ -113,49 +113,54 @@ console.log(req.body)
       emailHTML = `
       <div class="main">
         <center>
-          <h2 class="heading1">Welcome to GFG KARE STUDENT CHAPTER - Your Spot Confirmation!</h2>
+          <h1 class="heading1">Welcome to GFG KARE!</h1>
         </center>
         <div class="main2">
             <h3>Dear ${fullname},</h3>
-            <p>Thank you for registering with GFGKARE! We are thrilled to welcome you to our community of passionate and talented individuals.</p>
+            <p>You are successfully registered as a Student Member of GFG KARE for the academic year 2024 - 2025. We are thrilled to welcome you to a community of passionate, talented and like-minded individuals.</p>
             <p>We are pleased to confirm that you have opted for the role of Technical. This role plays a crucial part in our mission to enhance knowledge through mentorship. Here are the additional preferences links you have chosen:</p>
+            <p>Join the Technical WhatsApp group. More information regarding the enrollment will be shared soon.</p>
+            <h3><strong>WhatsApp Link:</strong></h3>
             ${additionalPreferencesHTML}
-            <h3><strong>Here’s what you can expect next:</strong></h3>
+            <h3><strong>Here's what you can expect after joining the group:</strong></h3>
             <ul>
               <li>Orientation Session: We will be hosting an orientation session. This will be a great opportunity for you to learn more about your role and meet fellow members.</li>
               <li>You will be assigned to a team of like-minded individuals. Your team lead will reach out to you shortly to introduce themselves and discuss upcoming sessions.</li>
             </ul>
-            <h3><strong>Stay Connected</strong></h3>
-            <p>Follow us on Social Media: <a href="https://www.linkedin.com/company/gfg-kare-student-chapter/posts/?feedView=all">Linkedin</a></p>
             <p>If you have any questions or need further assistance, please don’t hesitate to reach out to us at <a href="mailto:gfgkarestudentchapter@klu.ac.in">gfgkarestudentchapter@klu.ac.in</a> or visit our website <a href="https://gfgkare.github.io">gfgkare.github.io</a></p>
+            <h4><strong>Stay Connected :</strong></h4>
+            <a href="https://www.linkedin.com/company/gfg-kare-student-chapter/">Linkedin</a>
+            <a href="https://www.instagram.com/gfgkare?igsh=MXU5aGFkMGp6d21saw==">Instagram</a>
         </div>
         <div class="main">
-          <center><h1>Greetings from GFG TEAM - KARE</h1></center>
+          <center><h3>GFG KARE 💚</h3></center>
         </div>
       </div>`;
   } else {
       emailHTML = `
       <div class="main">
         <center>
-          <h2 class="heading1">Welcome to GFG KARE STUDENT CHAPTER - Your Spot Confirmation!</h2>
+          <h1 class="heading1">Welcome to GFG KARE!</h1>
         </center>
         <div class="main2">
             <h3>Dear ${fullname},</h3>
-            <p>Thank you for registering with GFGKARE! We are thrilled to welcome you to our community of passionate and talented individuals.</p>
+            <p>You are successfully registered as a Student Member of GFG KARE for the academic year 2024 - 2025. We are thrilled to welcome you to a community of passionate, talented and like-minded individuals.</p>
             <p>We are pleased to confirm that you have opted for the role of ${domain}. This role plays a crucial part in our mission to enhance knowledge through mentorship. Here is the WhatsApp group link:</p>
+            <p>Join the ${domain} WhatsApp group. More information regarding the enrollment will be shared soon.</p>
             <h3><strong>WhatsApp Link:</strong></h3>
             <a href="${whatsappLink}">Join here 👈</a>
-            <h3><strong>Here’s what you can expect next:</strong></h3>
+            <h3><strong>Here's what you can expect after joining the group:</strong></h3>
             <ul>
               <li>Orientation Session: We will be hosting an orientation session. This will be a great opportunity for you to learn more about your role and meet fellow members.</li>
               <li>You will be assigned to a team of like-minded individuals. Your team lead will reach out to you shortly to introduce themselves and discuss upcoming sessions.</li>
             </ul>
-            <h3><strong>Stay Connected</strong></h3>
-            <p>Follow us on Social Media: <a href="https://www.linkedin.com/company/gfg-kare-student-chapter/posts/?feedView=all">Linkedin</a></p>
             <p>If you have any questions or need further assistance, please don’t hesitate to reach out to us at <a href="mailto:gfgkarestudentchapter@klu.ac.in">gfgkarestudentchapter@klu.ac.in</a> or visit our website <a href="https://gfgkare.github.io">gfgkare.github.io</a></p>
+            <h4><strong>Stay Connected :</strong></h4>
+            <a href="https://www.linkedin.com/company/gfg-kare-student-chapter/">Linkedin</a>
+            <a href="https://www.instagram.com/gfgkare?igsh=MXU5aGFkMGp6d21saw==">Instagram</a>
         </div>
         <div class="main">
-          <center><h1>Greetings from GFG TEAM - KARE</h1></center>
+          <center><h3>GFG KARE 💚</h3></center>
         </div>
       </div>`;
   }
